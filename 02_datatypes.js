83cb4ce120ad732e24877a26cb7fe1603ea4d930
harshitshukla <harshitshukla6388@gmail.com>
2023-12-17 23:37:08 +0530

    data type

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js as newer version
+
+//alert ( 3 + 3) //we are using nodejs,not browser
+
+// code readability should be high
+
+let name = "harshit"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => value is not assined
+//symbol =>  for unique
+
+//imp null is a object 
+
+
+// object
+
+console.log(typeof null);  // object 
+console.log(typeof undefined)  // undefined
