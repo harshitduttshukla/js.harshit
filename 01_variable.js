8bccaaedb5c4246b2bc55725da1c070d71004c19
harshitshukla <harshitshukla6388@gmail.com>
2023-12-17 22:45:26 +0530

    js varable know

--- 01_variable.js
+++ 01_variable.js
@@ -1,3 +1,16 @@
 const accountId = 1234567
-accountId = 2
+let accountEmail = "hitesh@google.com"
+var accountPassword = "Jaipur"
+accountCity = "Jaipur"
+//accountId = 2 not allowed
+let accountstate
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
 console.log(accountId);
+
+
+/* prefer not use var
+beacuse of issue in block scope and functional scope */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
