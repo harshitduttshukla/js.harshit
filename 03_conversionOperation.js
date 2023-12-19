f2bab318307cf21bb424c368134b22522eaadb04
harshitshukla <harshitshukla6388@gmail.com>
2023-12-19 13:18:41 +0530

    conversion in detial

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -18,8 +18,43 @@ let valueInNumber = Number(score);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=>true;
 // 0 => false;
 // "harshirt" => true;
+
+
+
+//******************Operation*******************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.log(2**3);
+
+let str1 = "hello"
+let str2 = "harshit"
+
+
+//console.log("1" +2)
+//console.log(1 + "2")
+//console.log("1" + 2 + 2)
+//console.log(1 +2 + "2")
+
+//console.log((3 + 4) * 5 % 3);
+
+//console.log(-true);
+//console.log(+true);
+//console.log(+"");
+
+
+
+let nujm1, num2, num3,
+
+nukm1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter);
+
